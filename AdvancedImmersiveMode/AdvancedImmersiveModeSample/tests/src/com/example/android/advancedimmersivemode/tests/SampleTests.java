47ee9b46754f4244b94f52fdf79fe79ebbecdb8f
Tobias Preuss <tobias.preuss@googlemail.com>
2014-08-04 17:51:18 +0200

    Update projects.

--- AdvancedImmersiveMode/AdvancedImmersiveModeSample/tests/src/com/example/android/advancedimmersivemode/tests/SampleTests.java
+++ AdvancedImmersiveMode/AdvancedImmersiveModeSample/tests/src/com/example/android/advancedimmersivemode/tests/SampleTests.java
@@ -74,53 +74,8 @@ public class SampleTests extends ActivityInstrumentationTestCase2<MainActivity> 
         mTestFragment.mImmersiveModeCheckBox.setChecked(true);
         mTestFragment.mHideNavCheckbox.setChecked(true);
         mTestFragment.mHideStatusBarCheckBox.setChecked(true);
-        mTestFragment.toggleImmersiveMode();
+        mTestFragment.toggleUiFlags();
         int newUiFlags = getActivity().getWindow().getDecorView().getSystemUiVisibility();
         assertTrue("UI Flags didn't toggle.", uiFlags != newUiFlags);
     }
-
-    /**
-     * Verify that the view's height actually changed when the toggle method is called.
-     * This should result in a change in height for the DecorView.
-     */
-    public void testDecorHeightExpanded() {
-        // Grab the initial height of the DecorWindow.
-        int startingHeight = getActivity().getWindow().getDecorView().getHeight();
-
-        // In order to test that this worked:  Need to toggle the immersive mode on the UI thread,
-        // wait a suitable amount of time (this test goes with 200 ms), then check to see if the
-        // height changed.
-        try {
-            Runnable testRunnable = (new Runnable() {
-                public void run() {
-                    // Toggle immersive mode
-                    mTestFragment.mImmersiveModeCheckBox.setChecked(true);
-                    mTestFragment.mHideNavCheckbox.setChecked(true);
-                    mTestFragment.mHideStatusBarCheckBox.setChecked(true);
-                    mTestFragment.toggleImmersiveMode();
-                    synchronized(this) {
-                        // Notify any thread waiting on this runnable that it can continue
-                        this.notify();
-                    }
-                }
-            });
-            synchronized(testRunnable) {
-                // Since toggling immersive mode makes changes to the view hierarchy, it needs to run
-                // on the UI thread, or crashing will occur.
-                mTestActivity.runOnUiThread(testRunnable);
-                testRunnable.wait();
-
-            }
-            synchronized(this) {
-                //Wait about 200ms for the change to take place
-                wait(200L);
-            }
-        } catch (Throwable throwable) {
-            fail(throwable.getMessage());
-        }
-
-        int expandedHeight = getActivity().getWindow().getDecorView().getHeight();
-        assertTrue("Bars aren't hidden.", expandedHeight != startingHeight);
-    }
-
 }
